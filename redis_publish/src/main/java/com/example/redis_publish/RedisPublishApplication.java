24c0f9bcd4157a6f97508d92b23a40d75278352e
jemi180 <775734737@qq.com>
2022-09-14 10:59:49 +0800

    0914|添加redis发布代码

---
+++ redis_publish/src/main/java/com/example/redis_publish/RedisPublishApplication.java
@@ -0,0 +1,13 @@
+package com.example.redis_publish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisPublishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisPublishApplication.class, args);
+    }
+
+}
--- test/src/Test.java
+++ test/src/Test.java
@@ -1,19 +1,5 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("main start...");
-        Thread t = new Thread() {
-            public void run() {
-                System.out.println("thread run...");
-                try {
-                    Thread.sleep(20);
-                } catch (InterruptedException e) {}
-                System.out.println("thread end.");
-            }
-        };
-        t.start();
-        try {
-            Thread.sleep(10);
-        } catch (InterruptedException e) {}
-        System.out.println("main end...");
     }
 }
