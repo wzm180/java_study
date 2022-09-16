4a8880b6437079622ec35efb11f8a14886bd6a19
jemi180 <775734737@qq.com>
2022-09-16 23:27:14 +0800

    删除无用代码

--- test/src/Test2.java
+++
@@ -1,6 +0,0 @@
-public class Test2 {
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 3};
-        System.out.println(new int[]{1, 2, 3});
-    }
-}
--- test/src/extends_/Sub.java
+++
@@ -1,8 +0,0 @@
-package extends_;
-
-public class Sub extends Super{
-
-    public void print(){
-        System.out.println("" + n1 + n2 + n3 + getN4());
-    }
-}
